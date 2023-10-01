c171d49f73ed598330351bdeb2ba8ec8d9e57a32
tanishqmanuja <tanishqmanuja@gmail.com>
2023-10-02 00:01:11 +0530

    feat: better env at build time

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,5 +1,5 @@
 export default {
   PORT: process.env.PORT || 8080,
   LOCATION: process.env.LOCATION || "UNKNOWN",
-  INSTANCE_ID: process.env.INSTANCE_ID || "UNKNOWN",
+  INSTANCE_ID: process.env.INSTANCE_ID || crypto.randomUUID(),
 };
